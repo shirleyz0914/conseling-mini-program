f19336fee3c2adbe7cbfd4e010093d6495ae8d55
zhanghaoqi <shirley_z0914@163.com>
2022-03-24 19:14:34 +0800

    Initial Commit

---
+++ pages/Login/login.js
@@ -0,0 +1,99 @@
+import { setTokenStorage } from '../../utils/token'
+import logger from '../../utils/logger'
+import { genTestUserSig } from '../../debug/GenerateTestUserSig'
+
+const app = getApp()
+Page({
+  data: {
+    phone: '',
+    password: '',
+    hidden: false,
+    privateAgree: false,
+    code: '',
+    path: '',
+    lastTime: 0,
+    countryIndicatorStatus: false,
+    headerHeight: app.globalData.headerHeight,
+    statusBarHeight: app.globalData.statusBarHeight,
+  },
+  onLoad(option) {
+    this.setData({
+      path: option.path,
+    })
+    wx.setStorage({
+      key: 'path',
+      data: option.path,
+    })
+  },
+
+  onShow() {
+  },
+  // Token没过期可以利用Token登陆
+  loginWithToken() {
+    wx.switchTab({
+      url: '../TUI-Index/index',
+    })
+  },
+  // 回退
+  onBack() {
+    wx.navigateTo({
+      url: '../TUI-Index/TUI-Index',
+    })
+  },
+  // 输入手机号
+  bindPhoneInput(e) {
+    const val = e.detail.value
+    this.setData({
+      phone: val,
+    })
+  },
+  // 输入密码
+  bindPasswordInput(e) {
+    const val = e.detail.value;
+    this.setData({
+      password: val,
+    })
+  },
+  // 登录
+  login() {
+    const phone = this.data.phone;
+    const password = this.data.password;
+    const userSig = genTestUserSig(phone).userSig
+    logger.log(`TUI-login | login  | userSig:${userSig} userID:${phone}`)
+    app.globalData.userInfo = {
+      userSig,
+      userID,
+    }
+    setTokenStorage({
+      userInfo: app.globalData.userInfo,
+    })
+    if (this.data.path && this.data.path !== 'undefined') {
+      wx.redirectTo({
+        url: this.data.path,
+      })
+    } else {
+      wx.switchTab({
+        url: '../TUI-Index/index',
+      })
+    }
+  },
+  onAgreePrivateProtocol() {
+    this.setData({
+      privateAgree: !this.data.privateAgree,
+    })
+  },
+
+  linkToPrivacyTreaty() {
+    const url = 'https://web.sdk.qcloud.com/document/Tencent-IM-Privacy-Protection-Guidelines.html'
+    wx.navigateTo({
+      url: `../TUI-User-Center/webview/webview?url=${url}&nav=Privacy-Protection`,
+    })
+  },
+
+  linkToUserAgreement() {
+    const url = 'https://web.sdk.qcloud.com/document/Tencent-IM-User-Agreement.html'
+    wx.navigateTo({
+      url: `../TUI-User-Center/webview/webview?url=${url}&nav=User-Agreement`,
+    })
+  },
+})
