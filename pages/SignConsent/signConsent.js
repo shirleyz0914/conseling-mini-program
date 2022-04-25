a3ddec59505f2126a94b3c987cebaef59a8f1c4c
张灏琪 <shirley_z0914@163.com>
2022-04-25 15:01:30 +0800

    小程序端不再修改咨询师状态

--- pages/SignConsent/signConsent.js
+++ pages/SignConsent/signConsent.js
@@ -46,24 +46,24 @@ Page({
   },
   goCounseling() {
     const coun_id = wx.getStorageSync('coun_id');
-    wx.request({
-      url: 'http://1.15.129.51:3000/wx-users/changeCounsellorStauts',
-      method: 'PUT',
-      data: {
-        "coun_id": coun_id,
-        "coun_status": "busy"
-      },
-      success: (res) => {
-        if (res.statusCode === 200 && res.data.Code === 0) {
-          const payloadData = wx.getStorageSync('payloadData');
-          var times = Date.now();
-          var begin_time = new Date(times).toLocaleString('chinese', {hour12: false}).replaceAll('/', '-');
-          wx.setStorageSync('begin_time', begin_time);
-          wx.navigateTo({
-            url: `../../TUI-CustomerService/pages/TUI-Chat/chat?conversationInfomation=${JSON.stringify(payloadData)}`,
-          })
-        } 
-      }
-    });
+    // wx.request({
+    //   url: 'http://1.15.129.51:3000/wx-users/changeCounsellorStauts',
+    //   method: 'PUT',
+    //   data: {
+    //     "coun_id": coun_id,
+    //     "coun_status": "busy"
+    //   },
+    //   success: (res) => {
+    //     if (res.statusCode === 200 && res.data.Code === 0) {
+    const payloadData = wx.getStorageSync('payloadData');
+    var times = Date.now();
+    var begin_time = new Date(times).toLocaleString('chinese', {hour12: false}).replaceAll('/', '-');
+    wx.setStorageSync('begin_time', begin_time);
+    wx.navigateTo({
+      url: `../../TUI-CustomerService/pages/TUI-Chat/chat?conversationInfomation=${JSON.stringify(payloadData)}`,
+    })
+    //     } 
+    //   }
+    // });
   }
 })
